727a777d1b5cbc04d63a4ad6329f484145e77955
Shunya Kitada <syun.kitada@gmail.com>
2015-08-30 20:25:00 +0900

    updated

--- fabscript/openstack/mysql.py
+++ fabscript/openstack/mysql.py
@@ -1,14 +1,21 @@
 # coding: utf-8
 
-from fabkit import task
+from fabkit import task, parallel
 from fablib.mysql import MySQL
 
 mysql = MySQL()
 
 
 @task
-def setup():
+@parallel
+def setup0():
     mysql.setup()
+    return {'status': 1}
+
+
+@task
+def setup1_slave():
+    mysql.setup_slave()
     return {'status': 1}
 
 

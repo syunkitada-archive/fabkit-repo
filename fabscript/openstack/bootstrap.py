d1b17cfe454ff063a06a09805090498ec102cca0
Shunya Kitada <syun.kitada@gmail.com>
2015-09-21 23:11:23 +0900

    updated

--- fabscript/openstack/bootstrap.py
+++ fabscript/openstack/bootstrap.py
@@ -1,10 +1,11 @@
 # coding: utf-8
 
-from fabkit import task, Service, parallel
+from fabkit import task, Service, parallel, sudo
 
 
 @task
 @parallel
 def setup():
+    sudo('setenforce 0')
     Service('firewalld').stop().disable()
     return {'status': 1}

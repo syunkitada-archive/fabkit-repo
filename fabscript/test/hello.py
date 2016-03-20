ad5cf6e860bac4e9b64dfe384dda9ffbdc8ac495
Shunya Kitada <syun.kitada@gmail.com>
2016-03-20 22:32:04 +0900

    update

--- fabscript/test/hello.py
+++ fabscript/test/hello.py
@@ -1,10 +1,22 @@
 # coding: utf-8
 
-from fabkit import task, env
+from fabkit import task, env, run, sudo, Package, Service, filer
 
 
-@task(is_bootstrap=False)
+@task()
 def setup():
     print '{0}: hello'.format(env.host)
     print env.cluster
+    run('hostname')
+    sudo('hostname')
+    Package('memcached').install()
+    Service('memcached').start()
     # run('touch /tmp/test')
+    filer.file('/etc/memcached.conf')
+    filer.template('/etc/memcached.conf')
+
+
+@task()
+def local():
+    filer.file('/etc/memcached.conf')
+    filer.template('/etc/memcached.conf')
